f64b919f1fd13e130af457f55e7b9832d31f54bb
Eugene Sharygin <eush77@gmail.com>
2015-06-05 02:23:40 +0300

    Add --help and --version

--- cli.js
+++ cli.js
@@ -2,7 +2,20 @@
 'use strict';
 
 var npmMan = require('./'),
-    pager = require('default-pager');
+    pager = require('default-pager'),
+    help = require('help-version')(usage()).help;
+
+
+function usage() {
+  return 'Usage:  npm-man <package>';
+}
+
+
+(function (argv) {
+  if (argv.length != 1) {
+    help(argv.length);
+  }
+}(process.argv.slice(2)));
 
 
 npmMan(process.argv[2], function (err, man) {
