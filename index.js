afad8a4805f8ace467faacded122314f4c81e606
Eugene Sharygin <eush77@gmail.com>
2015-07-13 00:14:34 +0300

    Don't strip badges from Markdown output

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var registryUrl = require('registry-url')(),
     got = require('got'),
-    Mdast = require('mdast'),
+    MdAst = require('mdast'),
     mdastMan = require('mdast-man'),
     mdastStripBadges = require('mdast-strip-badges');
 
@@ -17,18 +17,19 @@ module.exports = function (name, opts, cb) {
     opts.man = true;
   }
 
-  var mdast = Mdast.use(mdastStripBadges);
+  var mdast = MdAst;
 
   got(registryUrl + name, { json: true }, function (err, pkg) {
     if (err) return cb(err);
 
     if (opts.man) {
-      mdast = mdast.use(mdastMan, {
-        section: 1,
-        name: pkg.name,
-        version: pkg.version,
-        description: pkg.description
-      });
+      mdast = mdast.use(mdastStripBadges)
+        .use(mdastMan, {
+          section: 1,
+          name: pkg.name,
+          version: pkg.version,
+          description: pkg.description
+        });
     }
 
     cb(err, mdast.process(pkg.readme));
