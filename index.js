08834d0c76f05b969ad44b2863340c8d4676715b
Eugene Sharygin <eush77@gmail.com>
2015-07-15 00:44:23 +0300

    Use readme-to-man-page@0.1.0

--- index.js
+++ index.js
@@ -2,49 +2,7 @@
 
 var registryUrl = require('registry-url')(),
     got = require('got'),
-    npmExpansion = require('npm-expansion'),
-    mdast = require('mdast'),
-    mdastFile = require('mdast/lib/file'),
-    mdastMan = require('mdast-man'),
-    mdastStripBadges = require('mdast-strip-badges'),
-    mdastSqueezeParagraphs = require('mdast-squeeze-paragraphs'),
-    mdAstVisit = require('mdast-util-visit'),
-    mdAstToString = require('mdast-util-to-string');
-
-
-var manPage = function (pkg) {
-  var ast = mdast()
-        .use(mdastStripBadges)
-        .use(mdastSqueezeParagraphs)
-        .run(mdast.parse(pkg.readme));
-
-  var description = pkg.description;
-
-  // Replace the default description with the content of the first
-  // paragraph of readme body since they are often identical.
-  mdAstVisit(ast, 'paragraph', function (node, index, parent) {
-    var firstParagraph = mdAstToString(node);
-    var prefix = firstParagraph.slice(0, description.length);
-    if (prefix.toLowerCase() == description.toLowerCase()) {
-      description = firstParagraph;
-      parent.children.splice(index, 1);
-    }
-    return false;
-  });
-
-  var manmd = mdast().use(mdastMan, {
-    section: 'npm',
-    manual: npmExpansion(),
-    name: pkg.name,
-    version: pkg.version,
-    description: description,
-    date: pkg.time.modified
-  });
-
-  // mdast-man captures some settings on the File object.
-  var file = mdastFile();
-  return manmd.stringify(manmd.run(ast, file), file, {});
-};
+    readmeToManPage = require('readme-to-man-page');
 
 
 module.exports = function (name, opts, cb) {
@@ -59,6 +17,6 @@ module.exports = function (name, opts, cb) {
 
   got(registryUrl + name, { json: true }, function (err, pkg) {
     if (err) return cb(err);
-    cb(err, opts.man ? manPage(pkg) : pkg.readme);
+    cb(err, opts.man ? readmeToManPage(pkg) : pkg.readme);
   });
 };
