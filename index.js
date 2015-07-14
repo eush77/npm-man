710c9a5d9c3da938f0eaeb61f5e64fec32f6d436
Eugene Sharygin <eush77@gmail.com>
2015-07-14 19:06:50 +0300

    Use first paragraph as a description if they match (close #2)

--- index.js
+++ index.js
@@ -2,10 +2,49 @@
 
 var registryUrl = require('registry-url')(),
     got = require('got'),
+    npmExpansion = require('npm-expansion'),
     mdast = require('mdast'),
+    mdastFile = require('mdast/lib/file'),
     mdastMan = require('mdast-man'),
     mdastStripBadges = require('mdast-strip-badges'),
-    npmExpansion = require('npm-expansion');
+    mdastSqueezeParagraphs = require('mdast-squeeze-paragraphs'),
+    mdAstVisit = require('mdast-util-visit'),
+    mdAstToString = require('mdast-util-to-string');
+
+
+var manPage = function (pkg) {
+  var ast = mdast()
+        .use(mdastStripBadges)
+        .use(mdastSqueezeParagraphs)
+        .run(mdast.parse(pkg.readme));
+
+  var description = pkg.description;
+
+  // Replace the default description with the content of the first
+  // paragraph of readme body since they are often identical.
+  mdAstVisit(ast, 'paragraph', function (node, index, parent) {
+    var firstParagraph = mdAstToString(node);
+    var prefix = firstParagraph.slice(0, description.length);
+    if (prefix.toLowerCase() == description.toLowerCase()) {
+      description = firstParagraph;
+      parent.children.splice(index, 1);
+    }
+    return false;
+  });
+
+  var manmd = mdast().use(mdastMan, {
+    section: 'npm',
+    manual: npmExpansion(),
+    name: pkg.name,
+    version: pkg.version,
+    description: description,
+    date: pkg.time.modified
+  });
+
+  // mdast-man captures some settings on the File object.
+  var file = mdastFile();
+  return manmd.stringify(manmd.run(ast, file), file, {});
+};
 
 
 module.exports = function (name, opts, cb) {
@@ -20,21 +59,6 @@ module.exports = function (name, opts, cb) {
 
   got(registryUrl + name, { json: true }, function (err, pkg) {
     if (err) return cb(err);
-
-    var md = mdast();
-
-    if (opts.man) {
-      md = md.use(mdastStripBadges)
-        .use(mdastMan, {
-          section: 'npm',
-          manual: npmExpansion(),
-          name: pkg.name,
-          version: pkg.version,
-          description: pkg.description,
-          date: pkg.time.modified
-        });
-    }
-
-    cb(err, md.process(pkg.readme));
+    cb(err, opts.man ? manPage(pkg) : pkg.readme);
   });
 };
